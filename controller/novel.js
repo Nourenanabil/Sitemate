0d54b75b7edecfa517a32319ee7a116b1f9be397
Nourena Nabil <nourenanabil@Karims-MacBook-Air.local>
2023-05-04 20:40:42 +1000

    add find and all functions for controller

--- controller/novel.js
+++ controller/novel.js
@@ -1,8 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+
 class Novel {
   constructor(id, title, description) {
-    this.id = id;
+    if (id) {
+      this.id = id;
+    } else {
+      const novels = Novel.all();
+      const sortedNovels = novels.sort((a, b) => {
+        a.id - b.id;
+      });
+      this.id = sortedNovels[sortedNovels.length - 1].id + 1;
+    }
+
     this.title = title;
     this.description = description;
+  }
+
+  static all() {
+    const novelsPath = path.join(__dirname, "../data/novels.json");
+    const novels = fs.readFileSync(novelsPath);
+    return JSON.parse(novels);
+  }
+
+  static find(id) {
+    const novels = this.all();
+    const novel = novels.find((n) => n.id == id);
+    return new Novel(novel.id, novel.title, novel.description);
   }
 
 }
