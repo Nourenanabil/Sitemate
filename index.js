a24913ed6631aa126dcb9dd38bb152b510801cd3
Nourena Nabil <nourenanabil@Karims-MacBook-Air.local>
2023-05-04 20:42:12 +1000

    add get and post API paths

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const Novel = require("./controller/novel.js");
 
 app.get("/", (req, res) => {
   res.send("Home page");
@@ -8,6 +9,33 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.get("/novels", (req, res) => {
+  try {
+    const novels = Novel.all();
+    res.json(novels);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+app.post("/novels", (req, res) => {
+  try {
+    console.log(req.body);
+    const { title, description } = req.body;
+    if (!title || !description) {
+      res.status(400).json({ error: "Missing required fields" });
+    } else {
+      const novel = new Novel(0, title, description);
+      novel.save();
+      res.status(201).json(novel);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "server error" });
+  }
+});
 
 
 app.listen(port, () => {
